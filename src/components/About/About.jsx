24f66e6bd6b5faab987e7ef4b5d38385dfee56d3
harshavr7 <146240198+harshavr7@users.noreply.github.com>
2025-05-15 21:00:05 +0530

    About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
             back-end development
           </p>
           <a
-            href="https://drive.google.com/file/d/1iNpS99SrJKimdWqAEkLYbFap2D7WnE0k/view?usp=sharing"
+            href="https://drive.google.com/file/d/1ERwodBDtYMaVTD4i1eoWGBoRQdOUJODX/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105 hover:cursor-pointer z-[999]"
